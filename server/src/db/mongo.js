478f1cabcff392753e1428b79e6fa8da28f29d86
Mary Welcey Guillen <welceyg@gmail.com>
2025-10-13 22:24:39 -0300

    refactor db connect fn

--- server/src/db/mongo.js
+++ server/src/db/mongo.js
@@ -1,9 +1,21 @@
 const { MongoClient } = require("mongodb");
+const client = new MongoClient(process.env.DB);
+let db;
 
-(async () => {
-  const client = new MongoClient(process.env.DB);
-  await client.connect();
-  console.log("Connected to MongoDB!");
-})().catch((err) => {
-  console.error("Error connecting to db: ", err);
-});
+async function connectDB() {
+  try {
+    await client.connect();
+    db = client.db(process.env.DB_NAME);
+    console.log("Connected to MongoDB!");
+    return db;
+  } catch (err) {
+    console.error("Error connecting to db: ", err);
+  }
+}
+
+function getDB() {
+  if (!db) throw new Error("DB not connected yet!");
+  return db;
+}
+
+module.exports = { connectDB, getDB };
--- server/src/index.js
+++ server/src/index.js
@@ -1,211 +1,215 @@
-require("./db/mongo.js");
-const express = require("express");
+const { connectDB } = require("./db/mongo");
 
-const http = require("http");
-const { Server } = require("socket.io");
-const { Observer, Emitter } = require("./Enum.js");
-const { getRandomColor, getInitials } = require("./utils.js");
+(async () => {
+  await connectDB();
+  
+  const express = require("express");
+  const http = require("http");
+  const { Server } = require("socket.io");
+  const { Observer, Emitter } = require("./Enum.js");
+  const { getRandomColor, getInitials } = require("./utils.js");
 
-const PORT = process.env.PORT || 5000;
-const app = express();
-const server = http.createServer(app);
+  const PORT = process.env.PORT || 5000;
+  const app = express();
+  const server = http.createServer(app);
 
-let users = [];
-let shoppingList = [];
-let buyingList = [];
-let doneList = [];
+  let users = [];
+  let shoppingList = [];
+  let buyingList = [];
+  let doneList = [];
 
-const io = new Server(server, {
-  cors: { origin: "*" },
-});
-
-io.on(Observer.CONNECTION, (socket) => {
-  socket.on(Observer.USER_JOINED, (data) => onUserJoined(socket, data));
-  socket.on(Observer.ADD_LOG, (msg) => onAddLog(socket.id, msg));
-
-  socket.on(Observer.ADD_SHOPPING_ITEM, (data) =>
-    onAddShoppingItem(socket.id, data)
-  );
-  socket.on(Observer.HAS_SHOPPING_LIST_UPDATE, (data) =>
-    onHasShoppingListUpdate(socket.id, data)
-  );
-  socket.on(Observer.HAS_BUYING_LIST_UPDATE, (data) =>
-    onHasBuyingListUpdate(socket.id, data)
-  );
-  socket.on(Observer.HAS_DONE_LIST_UPDATE, (data) =>
-    onHasDoneListUpdate(socket.id, data)
-  );
-  socket.on(Observer.EDIT_ITEM, (data) => onHasItemEditted(socket.id, data));
-
-  socket.on(Observer.ITEM_ASSIGNED_TO_USER, (data) =>
-    onAssignItemToUser(socket.id, data)
-  );
-
-  socket.on(Observer.DELETE_ITEM, (data) => onDeleteItem(socket.id, data));
-
-  socket.on(Observer.DISCONNECT, () => onDisconnect(socket.id));
-});
-
-function onUserJoined(socket, data) {
-  users.push({
-    id: socket.id,
-    ...data,
-    initials: getInitials(data.username),
-    color: getRandomColor(),
-    date: new Date(),
+  const io = new Server(server, {
+    cors: { origin: "*" },
   });
 
-  onAddLog(socket.id, `🙋 joined the session`);
+  io.on(Observer.CONNECTION, (socket) => {
+    socket.on(Observer.USER_JOINED, (data) => onUserJoined(socket, data));
+    socket.on(Observer.ADD_LOG, (msg) => onAddLog(socket.id, msg));
 
-  emit(socket, Emitter.ALERT, {
-    data: `👐 Welcome ${data.username}!`,
-    status: 1,
+    socket.on(Observer.ADD_SHOPPING_ITEM, (data) =>
+      onAddShoppingItem(socket.id, data)
+    );
+    socket.on(Observer.HAS_SHOPPING_LIST_UPDATE, (data) =>
+      onHasShoppingListUpdate(socket.id, data)
+    );
+    socket.on(Observer.HAS_BUYING_LIST_UPDATE, (data) =>
+      onHasBuyingListUpdate(socket.id, data)
+    );
+    socket.on(Observer.HAS_DONE_LIST_UPDATE, (data) =>
+      onHasDoneListUpdate(socket.id, data)
+    );
+    socket.on(Observer.EDIT_ITEM, (data) => onHasItemEditted(socket.id, data));
+
+    socket.on(Observer.ITEM_ASSIGNED_TO_USER, (data) =>
+      onAssignItemToUser(socket.id, data)
+    );
+
+    socket.on(Observer.DELETE_ITEM, (data) => onDeleteItem(socket.id, data));
+
+    socket.on(Observer.DISCONNECT, () => onDisconnect(socket.id));
   });
 
-  notify(Emitter.UPDATE_USER_LIST, users);
+  function onUserJoined(socket, data) {
+    users.push({
+      id: socket.id,
+      ...data,
+      initials: getInitials(data.username),
+      color: getRandomColor(),
+      date: new Date(),
+    });
 
-  //feed current data once logged in
-  notify(Emitter.UPDATE_SHOPPING_LIST, shoppingList);
-  notify(Emitter.UPDATE_BUYING_LIST, buyingList);
-  notify(Emitter.UPDATE_DONE_LIST, doneList);
-}
+    onAddLog(socket.id, `🙋 joined the session`);
 
-function onAddShoppingItem(id, data) {
-  shoppingList.push({
-    ...data,
-    id: crypto.randomUUID(),
-    createdAt: new Date(),
-  });
-  notify(Emitter.UPDATE_SHOPPING_LIST, shoppingList);
-  onAddLog(id, `👀 added item '${data.name}' to the list`);
-}
+    emit(socket, Emitter.ALERT, {
+      data: `👐 Welcome ${data.username}!`,
+      status: 1,
+    });
 
-function onHasShoppingListUpdate(id, data) {
-  shoppingList = data;
-  notify(Emitter.UPDATE_SHOPPING_LIST, shoppingList);
-}
+    notify(Emitter.UPDATE_USER_LIST, users);
 
-function onHasBuyingListUpdate(id, data) {
-  buyingList = data;
-  notify(Emitter.UPDATE_BUYING_LIST, buyingList);
-}
-
-function onAddLog(id, msg) {
-  notify(Emitter.UPDATE_LOGS, {
-    id: id,
-    userColor: getUserById(id)?.color ?? "#fff",
-    username: getUsernameById(id) ?? "",
-    message: msg,
-    timestamp: new Date(),
-  });
-}
-
-function onHasDoneListUpdate(id, data) {
-  doneList = data;
-  notify(Emitter.UPDATE_DONE_LIST, doneList);
-}
-
-function onHasItemEditted(socketId, data) {
-  const { type, id, name } = data;
-  const list = getListByType(type);
-
-  if (list.length) {
-    const item = list.find((x) => x.id == id);
-
-    if (item) {
-      const prev = item.name;
-      item.name = name;
-      notifyListByType(type);
-      onAddLog(socketId, `updated item '${prev}' to '${name}'`);
-    }
-  }
-}
-
-function onAssignItemToUser(socketId, data) {
-  const { id, type, assignedUser } = data;
-  const list = getListByType(type);
-  if (list.length) {
-    const item = list.find((x) => x.id == id);
-    if (item) {
-      item.assignedUser = assignedUser;
-
-      let msg = "";
-      if (assignedUser)
-        msg = `assigned item '${item.name}' to user ${
-          getUserById(assignedUser.id).username
-        }`;
-      else msg = `set item '${item.name}' as unassigned`;
-
-      notifyListByType(type);
-      onAddLog(socketId, msg);
-    }
-  }
-}
-
-function onDeleteItem(socketId, data) {
-  const { id, type } = data;
-  const list = getListByType(type);
-  if (list.length) {
-    const index = list.findIndex((x) => x.id == id);
-    if (index > -1) {
-      const prev = list[index].name;
-      list.splice(index, 1);
-      notifyListByType(type);
-      onAddLog(socketId, `deleted item '${prev}' from the list`);
-    }
-  }
-}
-
-function onDisconnect(id) {
-  if (getUserById(id)) {
-    onAddLog(id, `left the session 👋`);
-  }
-
-  users = users.filter((u) => u.id != id);
-  notify(Emitter.UPDATE_USER_LIST, users);
-}
-
-function getListByType(type) {
-  return type == "shoppingList"
-    ? shoppingList
-    : type == "buyingList"
-    ? buyingList
-    : type == "doneList"
-    ? doneList
-    : [];
-}
-
-function notifyListByType(type) {
-  if (type === "shoppingList")
+    //feed current data once logged in
     notify(Emitter.UPDATE_SHOPPING_LIST, shoppingList);
-  else if (type === "buyingList")
     notify(Emitter.UPDATE_BUYING_LIST, buyingList);
-  else if (type === "doneList") notify(Emitter.UPDATE_DONE_LIST, doneList);
-}
+    notify(Emitter.UPDATE_DONE_LIST, doneList);
+  }
 
-function getUsernameById(id) {
-  return users.find((u) => u.id == id)?.username ?? "";
-}
+  function onAddShoppingItem(id, data) {
+    shoppingList.push({
+      ...data,
+      id: crypto.randomUUID(),
+      createdAt: new Date(),
+    });
+    notify(Emitter.UPDATE_SHOPPING_LIST, shoppingList);
+    onAddLog(id, `👀 added item '${data.name}' to the list`);
+  }
 
-function getUserById(id) {
-  return users.find((u) => u.id == id);
-}
+  function onHasShoppingListUpdate(id, data) {
+    shoppingList = data;
+    notify(Emitter.UPDATE_SHOPPING_LIST, shoppingList);
+  }
 
-//sender only
-function emit(socket, event, data) {
-  socket.emit(event, data);
-}
+  function onHasBuyingListUpdate(id, data) {
+    buyingList = data;
+    notify(Emitter.UPDATE_BUYING_LIST, buyingList);
+  }
 
-//all clients and sender
-function notify(event, data) {
-  io.emit(event, data);
-}
+  function onAddLog(id, msg) {
+    notify(Emitter.UPDATE_LOGS, {
+      id: id,
+      userColor: getUserById(id)?.color ?? "#fff",
+      username: getUsernameById(id) ?? "",
+      message: msg,
+      timestamp: new Date(),
+    });
+  }
 
-//all clients except sender
-function broadcast(socket, event, data) {
-  socket.broadcast.emit(event, data);
-}
+  function onHasDoneListUpdate(id, data) {
+    doneList = data;
+    notify(Emitter.UPDATE_DONE_LIST, doneList);
+  }
 
-server.listen(PORT, () => {
-  console.warn(`Server running on http://localhost:${PORT}`);
-});
+  function onHasItemEditted(socketId, data) {
+    const { type, id, name } = data;
+    const list = getListByType(type);
+
+    if (list.length) {
+      const item = list.find((x) => x.id == id);
+
+      if (item) {
+        const prev = item.name;
+        item.name = name;
+        notifyListByType(type);
+        onAddLog(socketId, `updated item '${prev}' to '${name}'`);
+      }
+    }
+  }
+
+  function onAssignItemToUser(socketId, data) {
+    const { id, type, assignedUser } = data;
+    const list = getListByType(type);
+    if (list.length) {
+      const item = list.find((x) => x.id == id);
+      if (item) {
+        item.assignedUser = assignedUser;
+
+        let msg = "";
+        if (assignedUser)
+          msg = `assigned item '${item.name}' to user ${
+            getUserById(assignedUser.id).username
+          }`;
+        else msg = `set item '${item.name}' as unassigned`;
+
+        notifyListByType(type);
+        onAddLog(socketId, msg);
+      }
+    }
+  }
+
+  function onDeleteItem(socketId, data) {
+    const { id, type } = data;
+    const list = getListByType(type);
+    if (list.length) {
+      const index = list.findIndex((x) => x.id == id);
+      if (index > -1) {
+        const prev = list[index].name;
+        list.splice(index, 1);
+        notifyListByType(type);
+        onAddLog(socketId, `deleted item '${prev}' from the list`);
+      }
+    }
+  }
+
+  function onDisconnect(id) {
+    if (getUserById(id)) {
+      onAddLog(id, `left the session 👋`);
+    }
+
+    users = users.filter((u) => u.id != id);
+    notify(Emitter.UPDATE_USER_LIST, users);
+  }
+
+  function getListByType(type) {
+    return type == "shoppingList"
+      ? shoppingList
+      : type == "buyingList"
+      ? buyingList
+      : type == "doneList"
+      ? doneList
+      : [];
+  }
+
+  function notifyListByType(type) {
+    if (type === "shoppingList")
+      notify(Emitter.UPDATE_SHOPPING_LIST, shoppingList);
+    else if (type === "buyingList")
+      notify(Emitter.UPDATE_BUYING_LIST, buyingList);
+    else if (type === "doneList") notify(Emitter.UPDATE_DONE_LIST, doneList);
+  }
+
+  function getUsernameById(id) {
+    return users.find((u) => u.id == id)?.username ?? "";
+  }
+
+  function getUserById(id) {
+    return users.find((u) => u.id == id);
+  }
+
+  //sender only
+  function emit(socket, event, data) {
+    socket.emit(event, data);
+  }
+
+  //all clients and sender
+  function notify(event, data) {
+    io.emit(event, data);
+  }
+
+  //all clients except sender
+  function broadcast(socket, event, data) {
+    socket.broadcast.emit(event, data);
+  }
+
+  server.listen(PORT, () => {
+    console.warn(`Server running on http://localhost:${PORT}`);
+  });
+})();
